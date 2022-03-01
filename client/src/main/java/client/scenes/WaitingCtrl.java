77f6253ad9eb8108f61ca3b7ba9ae070f002d27b
Andrei Simionescu <a.simionescu@student.tudelft.nl>
2022-03-01 16:40:18 +0100

    add injectors and initializations methods to the Waiting Screen
    the injectors are just the columns from the tableView; the methods initialize and refresh[ the Player repo, while populating the waiting list table

--- client/src/main/java/client/scenes/WaitingCtrl.java
+++ client/src/main/java/client/scenes/WaitingCtrl.java
@@ -1,19 +1,35 @@
 package client.scenes;
 
 import client.utils.ServerUtils;
+import commons.Player;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.image.Image;
 import javafx.scene.text.Text;
 
 import javax.inject.Inject;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
 
-public class WaitingCtrl {
+public class WaitingCtrl implements Initializable {
 
     private final ServerUtils server;
     private final MainCtrl mainCtrl;
+    private ObservableList<Player> playerData;
 
     @FXML
-    private TableView tableView;
+    private TableView<Player> tableView;
+    @FXML
+    private TableColumn<Player, Image> avatarColumn;
+    @FXML
+    private TableColumn<Player, String> usernameColumn;
+
 
     @FXML
     private Text tip;
@@ -35,6 +51,28 @@ public class WaitingCtrl {
     public void load(){
         tip.setText("Theres only one correct answer per question, get the most right to win.");
         lobbyCode.setText(lobbyCode.getText() + 59864);
+        showActivePlayers();
+    }
+
+    public void showActivePlayers()
+    {
+        List<Player> activePlayers = server.getPlayers();
+
+        refresh();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources)
+    {
+        usernameColumn.setCellValueFactory(q -> new SimpleStringProperty(q.getValue().name));
+        //also initialization done for the avatar path/ logo directly
+    }
+
+    public void refresh()
+    {
+        var players = server.getPlayers();
+        playerData = FXCollections.observableList(players);
+        tableView.setItems(playerData);
     }
 
     public void leaveLobby(){
@@ -44,4 +82,6 @@ public class WaitingCtrl {
     public void startGame(){
         mainCtrl.showGameMCQ();
     }
+
+
 }
