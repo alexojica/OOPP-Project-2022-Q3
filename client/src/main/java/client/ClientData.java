3d451ccf66cbba0ea8458cb653d3238963ad74cc
Andrei Simionescu <a.simionescu@student.tudelft.nl>
2022-03-15 14:47:47 +0100

    Auto stash before merge of "63-game-functionality" and "origin/63-game-functionality"

---
+++ client/src/main/java/client/ClientData.java
@@ -0,0 +1,81 @@
+package client;
+
+import commons.Lobby;
+import commons.Player;
+import commons.Question;
+
+public class ClientData {
+
+    /**
+     * This class holds client data about the player that should remain accesible from the entire client
+     * It will also be more useful later on in case we have to do de/serialization, since this holds all player info
+     * Might also come in handy for cookie parsing, as this info should retain
+     */
+
+    private static Player clientPlayer;
+    private static Lobby clientLobby;
+    private static Long clientPointer;
+    private static Question clientQuestion;
+    private static Long clientScore = 0L;
+    private static Integer questionCounter = 0;
+    private static Boolean isHost = false; //remembers who the host of the lobby is
+
+    public static Boolean getIsHost() {
+        return isHost;
+    }
+
+    public static void setAsHost(Boolean value) {
+        isHost = value;
+    }
+
+    public static Integer getQuestionCounter()
+    {
+        return questionCounter;
+    }
+
+    public static void incrementQuestionCounter()
+    {
+        questionCounter++;
+    }
+
+    public static void setQuestionCounter(Integer counter)
+    {
+        questionCounter = counter;
+    }
+
+    public static Long getClientScore() {
+		return clientScore;
+	}
+
+    public static void setClientScore(Long score) {
+		clientScore = score;
+	}
+
+    public static void setPlayer(Player player)
+    {
+        clientPlayer = player;
+    }
+
+    public static Player getClientPlayer(){return clientPlayer;}
+
+    public static void setPointer(Long pointer)
+    {
+        clientPointer = pointer;
+    }
+
+    public static Long getClientPointer(){return clientPointer;}
+
+    public static void setQuestion(Question question)
+    {
+        clientQuestion = question;
+    }
+
+    public static Question getClientQuestion(){return clientQuestion;}
+
+    public static void setLobby(Lobby lobby)
+    {
+        clientLobby = lobby;
+    }
+
+    public static Lobby getClientLobby(){return clientLobby;}
+}
