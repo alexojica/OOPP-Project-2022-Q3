c9d42cab3e358113c3d589e9ccbe9a5154eba7ac
Thys Arre Kok <T.A.Kok@student.tudelft.nl>
2022-03-15 20:01:32 +0100

    Removed unused imports
    Checkstyle fixes

--- client/src/main/java/client/utils/ServerUtils.java
+++ client/src/main/java/client/utils/ServerUtils.java
@@ -20,12 +20,9 @@ import commons.Lobby;
 import commons.Player;
 import commons.Question;
 import constants.ConnectionStatusCodes;
+
 import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
 import java.util.List;
 
 import commons.LeaderboardEntry;
@@ -34,11 +31,6 @@ import org.glassfish.jersey.client.ClientConfig;
 import jakarta.ws.rs.client.ClientBuilder;
 import jakarta.ws.rs.client.Entity;
 import jakarta.ws.rs.core.GenericType;
-import org.glassfish.jersey.client.ClientConfig;
-
-import java.util.List;
-
-import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
 
 public class ServerUtils {
 
