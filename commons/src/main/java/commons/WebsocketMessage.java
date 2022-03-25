0830c579b4664420d6344b8a30c25817747decf7
Thys Arre Kok <T.A.Kok@student.tudelft.nl>
2022-03-25 11:19:59 +0100

    Add tests for certain methods from websocketmessage

--- commons/src/main/java/commons/WebsocketMessage.java
+++ commons/src/main/java/commons/WebsocketMessage.java
@@ -4,15 +4,9 @@ import constants.JokerType;
 import constants.QuestionTypes;
 import constants.ResponseCodes;
 
+import java.util.Objects;
+
 public class WebsocketMessage {
-
-    public ResponseCodes getCode() {
-        return code;
-    }
-
-    public String getLobbyToken() {
-        return lobbyToken;
-    }
 
     private ResponseCodes code;
 
@@ -58,6 +52,29 @@ public class WebsocketMessage {
         return questionType;
     }
 
+    public ResponseCodes getCode() {
+        return code;
+    }
+
+    public String getLobbyToken() {
+        return lobbyToken;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        WebsocketMessage that = (WebsocketMessage) o;
+        return code == that.code && questionType == that.questionType && Objects.equals(lobbyToken, that.lobbyToken) &&
+                Objects.equals(message, that.message) && Objects.equals(question, that.question) &&
+                Objects.equals(pointer, that.pointer) && Objects.equals(player, that.player) &&
+                jokerType == that.jokerType;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(code, questionType, lobbyToken, message, question, pointer, player, jokerType);
+    }
 
     /**
      * Constructor for start game
